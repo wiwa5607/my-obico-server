af1e899bdacf784f112bfcdaecd7960bf8fb9a4b
Will Walker <willwalker1226@gmail.com>
2022-08-23 18:51:57 -0600

    change executable

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,3 @@
-pipenv run manage.py migrate && \
-pipenv run manage.py collectstatic -v 2 --noinput && \
-pipenv run manage.py runserver --nostatic --noreload 0.0.0.0:3334
+pipenv run python manage.py migrate && \
+pipenv run python manage.py collectstatic -v 2 --noinput && \
+pipenv run pyton manage.py runserver --nostatic --noreload 0.0.0.0:3334
