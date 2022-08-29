b592b6bdac8654b11e0e388f995d8df05036371f
Will Walker <willwalker1226@gmail.com>
2022-08-28 20:22:53 -0600

    correct frontend copy path

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,3 @@
 pipenv run python manage.py migrate && \
-ls -ltra  && ls -ltra frontend && \
 pipenv run python manage.py collectstatic -v 2 --noinput && \
 pipenv run python manage.py runserver --nostatic --noreload 0.0.0.0:3334
