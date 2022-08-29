27ea8bb3e8c03a7f6b22d19271bd5a32d11faac1
Will Walker <willwalker1226@gmail.com>
2022-08-28 20:20:03 -0600

    add debug to run script

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,4 @@
 pipenv run python manage.py migrate && \
+ls -ltra  && ls -ltra fronted && \
 pipenv run python manage.py collectstatic -v 2 --noinput && \
 pipenv run python manage.py runserver --nostatic --noreload 0.0.0.0:3334
