ba1a3fc606a884d90daa3e1ed6c480d7ffbae85a
Will Walker <willwalker1226@gmail.com>
2022-08-28 20:20:59 -0600

    typo

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 pipenv run python manage.py migrate && \
-ls -ltra  && ls -ltra fronted && \
+ls -ltra  && ls -ltra frontend && \
 pipenv run python manage.py collectstatic -v 2 --noinput && \
 pipenv run python manage.py runserver --nostatic --noreload 0.0.0.0:3334
