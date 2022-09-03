99a821a7804a2fd69f8eba0f228c39fde98778a1
Will Walker <willwalker1226@gmail.com>
2022-09-02 22:17:16 -0600

    user updates and path append

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,3 @@
-pipenv run python manage.py migrate && \
-pipenv run python manage.py collectstatic -v 2 --noinput && \
-pipenv run python manage.py runserver --nostatic --noreload 0.0.0.0:3334
+python manage.py migrate && \
+python manage.py collectstatic -v 2 --noinput && \
+python manage.py runserver --nostatic --noreload 0.0.0.0:3334
