28675a37bd34781d1877b26ba62bc599675b8f3b
Will Walker <willwalker1226@gmail.com>
2022-09-04 03:00:42 +0000

    Update backend/run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,4 @@
+cd /app
 pipenv run python manage.py migrate && \
 pipenv run python manage.py collectstatic -v 2 --noinput && \
 pipenv run python manage.py runserver --nostatic --noreload 0.0.0.0:3334
